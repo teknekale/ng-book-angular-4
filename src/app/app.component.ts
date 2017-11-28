4af255c0e7e7ea29f51641d5c5c5251c8beaefbd
Simone Pachera <simone.pachera@leotech.com.sg>
2017-11-28 14:41:12 +0000

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`new article: ${title.value} - ${link.value}`);
+    return false;
+  }
+}
